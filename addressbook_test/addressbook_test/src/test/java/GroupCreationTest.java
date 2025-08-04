51685ff6115a7650b5dfb35d9ce9cb76ff5862e8
kulichsss <dankul10@yandex.ru>
2025-08-04 22:39:00 +0300

    Добавление базового класса

--- addressbook_test/addressbook_test/src/test/java/GroupCreationTest.java
+++ addressbook_test/addressbook_test/src/test/java/GroupCreationTest.java
@@ -1,35 +1,13 @@
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
 
-public class GroupCreationTest {
-    private WebDriver driver;
-    private GroupHelper groupHelper;
-
-
-    @BeforeEach
-    public void setUp() {
-        if (driver == null) {
-            driver = new FirefoxDriver();
-            Runtime.getRuntime().addShutdownHook(new Thread(driver::quit));
-            driver.get("http://localhost/addressbook/addressbook/");
-            driver.manage().window().setSize(new Dimension(1132, 1062));
-            driver.findElement(By.name("user")).sendKeys("admin");
-            driver.findElement(By.name("pass")).sendKeys("secret");
-            driver.findElement(By.xpath("//input[@value=\'Login\']")).click();
-        }
-        groupHelper = new GroupHelper(driver);
-
-
-    }
-
+public class GroupCreationTest extends TestBase{
 
     @Test
     public void canCreateGroup() {
-        groupHelper.createGroup();
+        openGroupPage();
+        createGroup("Name1", "Logo header","Comment footer");
     }
+
+
 }
