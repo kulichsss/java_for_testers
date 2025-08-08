f4f3729bd2f01fa83930e79e801117c43d9aa41a
kulichsss <dankul10@yandex.ru>
2025-08-08 22:21:18 +0300

    Добавление базового класса HelperBase

--- addressbook_test/addressbook_test/src/test/java/manager/GroupHelper.java
+++ addressbook_test/addressbook_test/src/test/java/manager/GroupHelper.java
@@ -3,17 +3,15 @@ package manager;
 import model.GroupData;
 import org.openqa.selenium.By;
 
-public class GroupHelper {
-
-    private final ApplicationManager manager;
+public class GroupHelper  extends HelperBase {
 
     public GroupHelper(ApplicationManager manager) {
-        this.manager = manager;
+        super(manager);
     }
 
     public void openGroupPage() {
         if (!manager.isElementPresent(By.name("new"))) {
-            manager.driver.findElement(By.linkText("groups")).click();
+            click(By.linkText("groups"));
         }
     }
 
@@ -24,21 +22,19 @@ public class GroupHelper {
 
     public void deleteGroup() {
         openGroupPage();
-        manager.driver.findElement(By.xpath("//input[@name='selected[]']")).click();
-        manager.driver.findElement(By.name("delete")).click();
-        manager.driver.findElement(By.linkText("group page")).click();
+        click(By.xpath("//input[@name='selected[]']"));
+        click(By.name("delete"));
+        click(By.linkText("group page"));
     }
 
     public void createGroup(GroupData group) {
         openGroupPage();
-        manager.driver.findElement(By.name("new")).click();
-        manager.driver.findElement(By.name("group_name")).click();
-        manager.driver.findElement(By.name("group_name")).sendKeys(group.name());
-        manager.driver.findElement(By.name("group_header")).click();
-        manager.driver.findElement(By.name("group_header")).sendKeys(group.header());
-        manager.driver.findElement(By.name("group_footer")).click();
-        manager.driver.findElement(By.name("group_footer")).sendKeys(group.footer());
-        manager.driver.findElement(By.name("submit")).click();
-        manager.driver.findElement(By.linkText("group page")).click();
+        click(By.name("new"));
+        type(By.name("group_name"), group.name());
+        type(By.name("group_header"), group.header());
+        type(By.name("group_footer"), group.footer());
+        click(By.name("submit"));
+        click(By.linkText("group page"));
     }
+
 }
